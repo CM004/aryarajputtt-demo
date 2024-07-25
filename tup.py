d53b6cac6e9bd1f0df5fa1b954f798eb8f4e4eb0
Aryaranjan13 <aryaaranjann@gmail.com>
2024-07-25 12:50:39 +0530

    Add files via upload

---
+++ tup.py
@@ -0,0 +1,35 @@
+'''test_list = [(1, 4, 5), (7, 8), (2, 4, 10)] 
+ 
+# printing original list 
+print("The original list is : " + str(test_list)) 
+ 
+# Average of tuple list 
+# Using loops 
+sum = 0
+for sub in test_list: 
+    for i in sub: 
+        sum = sum + i 
+res = sum / len(test_list) 
+ 
+# printing result 
+print("The mean of tuple list is : " + str(res)) '''
+
+
+'''t=[(23,1,1,4,2,6,7)]
+print("original list:",t)
+
+sum=0
+for sub in t:
+ for i in sub:
+    sum= sum + i
+res=sum/len(t)
+print("the mean of tuple is:",res)'''
+
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number: "))
+c=a+b
+d=a-b
+x=a*b
+print("sum",c)
+print("diff",d)
+print("prod",x)
